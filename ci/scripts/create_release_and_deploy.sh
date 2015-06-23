dddf2976464eab5afe39d6d7514d143ea4adf97d
Dr Nic Williams <drnicwilliams@gmail.com>
2015-06-23 10:32:40 -0400

    fail fast

--- ci/scripts/create_release_and_deploy.sh
+++ ci/scripts/create_release_and_deploy.sh
@@ -4,6 +4,8 @@ _bosh() {
   bosh -n -t ${bosh_target} -u ${bosh_username} -p ${bosh_password} $@
 }
 
+set -e
+
 _bosh delete deployment ${bosh_deployment_name} --force || echo "Continuing..."
 _bosh create release
 _bosh upload release --rebase || true
