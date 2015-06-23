ea370d48eafb85eaeb97b85819db2ec7555d7a82
Dr Nic Williams <drnicwilliams@gmail.com>
2015-06-23 10:20:32 -0400

    create dev release & upload

---
+++ ci/scripts/create_release_and_deploy.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+_bosh() {
+  bosh -n -t ${bosh_target} -u ${bosh_username} -p ${bosh_password} $@
+}
+
+_bosh delete deployment ${bosh_deployment_name} --force || echo "Continuing..."
+_bosh create release
+_bosh upload release --rebase || true
--- ci/scripts/delete_existing_deployment.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-bosh -n -t ${bosh_target} -u ${bosh_username} -p ${bosh_password} \
-  delete deployment ${bosh_deployment_name} --force || echo "Continuing..."
