f0de59c767aeed79be345bf8e307d9e2b07fc98c
Dr Nic Williams <drnicwilliams@gmail.com>
2015-07-16 07:56:39 -0700

    Squashed 'src/rdpg-acceptance-tests/' changes from a73e8ea..27d4ecc
    27d4ecc Merge pull request #6 from starkandwayne/split_out_postgres_consul_check
    64b40b2 Split out postgresql consul check (node count)
    
    git-subtree-dir: src/rdpg-acceptance-tests
    git-subtree-split: 27d4ecc20151ad956840599190d571441a179c84

--- README.md
+++ README.md
@@ -1,185 +1,37 @@
-Reliable Distributed PostgreSQL (RDPG) BOSH release
-===================================================
+RDPG Acceptance Tests
+=====================
 
--	Build pipeline http://54.82.85.58:8080/pipelines/rdpg-boshrelease (see [Pipeline](#pipeline) for more)
+This project contains acceptance/smoke tests for the [Reliable Distributed Postgres project](https://github.com/starkandwayne/rdpg-boshrelease) (RDPG).
 
-License
+Prerequisites
+-------------
+
+```
+go get ./...
+```
+
+Running
 -------
 
-MIT, see LICENSE file.
+Create a `config.json` file and set the path to it in `$CONFIG`, then run the test suites.
 
-Usage: Configuration & Delpoyment
----------------------------------
-
-We will walk through an example of using with Cloud Foundry.
-
-Be sure to first target your BOSH Director:
-
-```sh
-bosh target $BOSH_HOST
+```
+mkdir tmp
+cat > tmp/config.json <<EOS
+{}
+EOS
+export CONFIG=$PWD/tmp/config.json
 ```
 
-If you are using bosh-lite you target like so:
+Then run tests:
 
-```sh
-bosh target 192.168.50.4 lite
+```
+./bin/test-acceptance
 ```
 
-Now clone this release and cd into the directory:
+Update godeps
+-------------
 
-```sh
-git clone https://.../rdpg-boshrelease.git
-cd rdpg-boshrelease
 ```
-
-If you intend on using a final release upload it like so:
-
-```sh
-bosh upload release releases/rdpg-1.yml
+godep save ./...
 ```
-
-Next download your manifest file for the deployment targeted so we can edit it and add the release.
-
-```sh
-mkdir -p ~/workspace/manifests
-bosh download manifest rdpg-development ~/workspace/manifests/rdpg.yml
-bosh deployment ~/workspace/manifests/rdpg.yml
-```
-
-Alternatively, you can make your manifest. For example to prepare a manifest for bosh-lite (warden) using the 'centos' stemcell we would do the following:
-
-```sh
-STEMCELL_OS=centos ./rdpg-dev manifest warden
-```
-
-Edit the manifest file you downloaded (`~/workspace/manifests/rdpg.yml`) and add settings as follows.
-
-Add to the list of known `releases:`
-
-```yaml
-releases:
-#...
-- name: rdpg
-  version: latest
-```
-
-For consistency also add to the `releases:` section under `meta:`
-
-```yaml
-meta:
-  environment: rdpg-development
-  releases:
-  - name: cf
-    version: latest
-  - name: rdpg
-    version: latest
-```
-
-Add properties such as tags.
-
-```yaml
-properties:
-# ... lots of properties ... at bottom put vv
-  rdpg:
-```
-
-Now, for every `instances:` entry you wish to collocate this release with under `jobs:` add the following in the `templates:` section:
-
-```yaml
-  - name: rdpg
-    release: rdpg
-```
-
-Now you can deploy,
-
-```yaml
-bosh -n deploy
-```
-
-Note that for each job you create in your release that you want to run on a Job VM you must add a `templates:` entry with the `name:` of the template and the `release:` from which it comes.
-
-Deployments Blobs
------------------
-
-The script `./rdpg-dev blobs` is used to prepare the `blobs/` directory runs each package's `prepare` script from within the `blobs/` directory. This will run each package's `prepare` script, if it exists, which *should* download and prepare that package's required blobs (source tarballs, etc...) into the {package}/ directory.
-
-Development
------------
-
-Download your manifest file for the deployment targeted.
-
-Target your BOSH Director as explained above, if you already have a deploy be sure to also download your manifest as per above.
-
-If this is your first time cloning the release repository for develompent first prepare all of the things:
-
-```sh
-./rdpg-dev prepare warden
-```
-
-This is equivalent to:
-
-```sh
-./rdpg-dev blobs
-./rdpg-dev release
-./rdpg-dev stemcell warden
-./rdpg-dev manifest warden
-```
-
-Once these steps have all been completed
-
-```sh
-bosh -n deploy
-```
-
-Debugging & QA
---------------
-
-See `docs/notes.md` for more information.
-
-In order to gain access to one of the VMs, from a terminal `bosh ssh rdpg {VM Index}`, for example to ssh to the first node:
-
-```sh
-bosh ssh rdpg 0 # Hop on and have a look around...
-```
-
-If you want to destroy and recreate on specific node, say `rdpg/0`, you do the following:
-
-```sh
-bosh -n recreate rdpg 0 --force
-```
-
-See `docs/rdpg.md` for release specific information.
-
-To run acceptance tests:
-
-`bosh create release --force && bosh upload release && bosh -n deploy && bosh run errand acceptance_tests`
-
-Pipeline
---------
-
-This BOSH release is automatically built and tested upon every commit to master.
-
-![bosh release changes trigger deploy job](http://cl.ly/image/0O220s281l1L/bosh_release_changes_trigger_deploy_job.png)
-
-The `deploy` job will:
-
-1.	Run `bosh create release` to ensure that the BOSH release can be successfully created
-2.	Upload the release to a bosh-lite running on AWS
-3.	Generate a manifest and deploy a cluster (via the `./rdpg-dev manifest warden` manifest generator)
-4.	Run the `acceptance_tests` errand
-
-The `deploy` job is triggered when:
-
--	`jobs` or `packages` folders are modified - changing the BOSH release itself
--	`templates` folders are modified - changing how users might deploy the BOSH release
--	`src` folder is modified (see below for jobs that do this via Concourse)
--	`config/blobs.yml` is modified - changing the blobs that go into the BOSH release
-
-There are some feeder repositories that are also monitored by the Concourse pipeline:
-
--	[rdpgd](https://github.com/starkandwayne/rdpgd) via `bump-rdpgd` job - the bespoke agent/service broker, written in Go
--	[rdpg-acceptance-tests](https://github.com/starkandwayne/rdpg-acceptance-tests) via `bump-tests` job - a set of acceptance tests for a running cluster of RDPG, written in Go
-
-If these repositories change (`master` branch) then they are added into the `src/` folder (via `git subtree pull`) and committed. This will then trigger the `deploy` job above.
-
-![repos-feeding](http://cl.ly/image/1z210F0f3o3K/pasted_image_at_2015_07_09_10_08_pm.png)
---
+++ rdpg-service/postgres/postgres_test.go
@@ -0,0 +1,374 @@
+package postgres_test
+
+import (
+	"fmt"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	"github.com/wayneeseguin/rdpgd/pg"
+
+	consulapi "github.com/hashicorp/consul/api"
+	"github.com/starkandwayne/rdpg-acceptance-tests/helpers"
+)
+
+func getRowCount(address string, sq string) (queryRowCount int, err error) {
+	p := pg.NewPG(address, "7432", `rdpg`, `rdpg`, "admin")
+	db, err := p.Connect()
+	if err != nil {
+		return 0, err
+	}
+	var rowCount []int
+	err = db.Select(&rowCount, sq)
+	if err != nil {
+		return 0, err
+	}
+	return rowCount[0], nil
+}
+
+func getAllNodes() (allNodes []*consulapi.CatalogService) {
+
+	consulConfig := consulapi.DefaultConfig()
+	consulConfig.Address = helpers.TestConfig.ConsulIP
+	consulClient, _ := consulapi.NewClient(consulConfig)
+
+	rdpgsc1Nodes, _, _ := consulClient.Catalog().Service("rdpgsc1", "", nil)
+	rdpgsc2Nodes, _, _ := consulClient.Catalog().Service("rdpgsc2", "", nil)
+	nodes, _, _ := consulClient.Catalog().Service("rdpgmc", "", nil)
+	for index := range rdpgsc1Nodes {
+		nodes = append(nodes, rdpgsc1Nodes[index])
+	}
+	for index := range rdpgsc2Nodes {
+		nodes = append(nodes, rdpgsc2Nodes[index])
+	}
+
+	return nodes
+}
+
+func getNodesByClusterName(clusterName string) (allNodes []*consulapi.CatalogService) {
+
+	consulConfig := consulapi.DefaultConfig()
+	consulConfig.Address = helpers.TestConfig.ConsulIP
+	consulClient, _ := consulapi.NewClient(consulConfig)
+
+	nodes, _, _ := consulClient.Catalog().Service(clusterName, "", nil)
+
+	return nodes
+
+}
+
+func getServiceNodes() (allServiceNodes []*consulapi.CatalogService) {
+
+	consulConfig := consulapi.DefaultConfig()
+	consulConfig.Address = helpers.TestConfig.ConsulIP
+	consulClient, _ := consulapi.NewClient(consulConfig)
+
+	nodes, _, _ := consulClient.Catalog().Service("rdpgsc1", "", nil)
+	rdpgsc2Nodes, _, _ := consulClient.Catalog().Service("rdpgsc2", "", nil)
+	for index := range rdpgsc2Nodes {
+		nodes = append(nodes, rdpgsc2Nodes[index])
+	}
+
+	return nodes
+}
+
+var _ = Describe("RDPG Postgres Testing...", func() {
+
+	It("Check Schemas Exist", func() {
+
+		allNodes := getAllNodes()
+
+		//Check all nodes
+		var nodeRowCount []int
+		for i := 0; i < len(allNodes); i++ {
+			address := allNodes[i].Address
+			sq := ` SELECT count(schema_name) as rowCount FROM information_schema.schemata WHERE schema_name IN ('bdr', 'rdpg', 'cfsb', 'tasks', 'backups', 'metrics', 'audit'); `
+			rowCount, err := getRowCount(address, sq)
+			nodeRowCount = append(nodeRowCount, rowCount)
+			Expect(err).NotTo(HaveOccurred())
+			fmt.Printf("%s: Found %d schemas in rdpg database...\n", allNodes[i].Node, rowCount)
+		}
+		//Verify each database also sees the same number of records (bdr sanity check)
+		fmt.Printf("%#v\n", nodeRowCount)
+
+		for i := 1; i < len(nodeRowCount); i++ {
+			Expect(nodeRowCount[0]).To(Equal(nodeRowCount[i]))
+		}
+
+		Expect(len(nodeRowCount)).NotTo(Equal(0))
+		Expect(nodeRowCount[0]).To(Equal(7))
+	})
+
+	It("Check cfsb Tables Exist", func() {
+
+		allNodes := getAllNodes()
+
+		//Check all nodes
+		var nodeRowCount []int
+		for i := 0; i < len(allNodes); i++ {
+			address := allNodes[i].Address
+			sq := ` SELECT count(table_name) as rowCount FROM information_schema.tables WHERE table_schema = 'cfsb' and table_name IN ('services','plans','instances','bindings','credentials'); `
+			rowCount, err := getRowCount(address, sq)
+			nodeRowCount = append(nodeRowCount, rowCount)
+			Expect(err).NotTo(HaveOccurred())
+			fmt.Printf("%s: Found %d tables in schema cfsb...\n", allNodes[i].Node, rowCount)
+		}
+
+		//Verify each database also sees the same number of records (bdr sanity check)
+		for i := 1; i < len(nodeRowCount); i++ {
+			Expect(nodeRowCount[0]).To(Equal(nodeRowCount[i]))
+		}
+
+		Expect(len(nodeRowCount)).NotTo(Equal(0))
+		Expect(nodeRowCount[0]).To(Equal(5))
+	})
+
+	It("Check rdpg Tables Exist", func() {
+
+		allNodes := getAllNodes()
+
+		//Check all nodes
+		var nodeRowCount []int
+		for i := 0; i < len(allNodes); i++ {
+			address := allNodes[i].Address
+			sq := ` SELECT count(table_name) as rowCount FROM information_schema.tables WHERE table_schema = 'rdpg' and table_name IN ('config', 'consul_watch_notifications', 'events'); `
+			rowCount, err := getRowCount(address, sq)
+			nodeRowCount = append(nodeRowCount, rowCount)
+			Expect(err).NotTo(HaveOccurred())
+			fmt.Printf("%s: Found %d tables in schema rdpg...\n", allNodes[i].Node, rowCount)
+		}
+
+		//Verify each database also sees the same number of records (bdr sanity check)
+		for i := 1; i < len(nodeRowCount); i++ {
+			Expect(nodeRowCount[0]).To(Equal(nodeRowCount[i]))
+		}
+
+		Expect(len(nodeRowCount)).NotTo(Equal(0))
+		Expect(nodeRowCount[0]).To(Equal(3))
+
+	})
+
+	It("Check tasks Tables Exist", func() {
+		allNodes := getAllNodes()
+
+		//Check all nodes
+		var nodeRowCount []int
+		for i := 0; i < len(allNodes); i++ {
+			address := allNodes[i].Address
+			sq := ` SELECT count(table_name) as rowCount FROM information_schema.tables WHERE table_schema = 'tasks' and table_name IN ('tasks','schedules'); `
+			rowCount, err := getRowCount(address, sq)
+			nodeRowCount = append(nodeRowCount, rowCount)
+			Expect(err).NotTo(HaveOccurred())
+			fmt.Printf("%s: Found %d tables in schema tasks...\n", allNodes[i].Node, rowCount)
+		}
+		//Verify each database also sees the same number of records (bdr sanity check)
+		for i := 1; i < len(nodeRowCount); i++ {
+			Expect(nodeRowCount[0]).To(Equal(nodeRowCount[i]))
+		}
+
+		Expect(len(nodeRowCount)).NotTo(Equal(0))
+		Expect(nodeRowCount[0]).To(Equal(2))
+
+	})
+
+	It("Check Instance Counts", func() {
+
+		rdpgmcNodes := getNodesByClusterName("rdpgmc")
+		rdpgsc1Nodes := getNodesByClusterName("rdpgsc1")
+		rdpgsc2Nodes := getNodesByClusterName("rdpgsc2")
+
+		//Check SC1
+		var sc1InstanceCount []int
+		for i := 0; i < len(rdpgsc1Nodes); i++ {
+			address := rdpgsc1Nodes[i].Address
+			sq := ` SELECT count(*) as instance_count FROM cfsb.instances WHERE effective_at IS NOT NULL AND decommissioned_at IS NULL; `
+			rowCount, err := getRowCount(address, sq)
+			sc1InstanceCount = append(sc1InstanceCount, rowCount)
+			Expect(err).NotTo(HaveOccurred())
+			fmt.Printf("%s: Found %d instances...\n", rdpgsc1Nodes[i].Node, rowCount)
+		}
+		//Verify each database also sees the same number of records (bdr sanity check)
+		for i := 1; i < len(sc1InstanceCount); i++ {
+			Expect(sc1InstanceCount[0]).To(Equal(sc1InstanceCount[i]))
+		}
+
+		//Check SC2
+		var sc2InstanceCount []int
+		for i := 0; i < len(rdpgsc2Nodes); i++ {
+			address := rdpgsc2Nodes[i].Address
+			sq := ` SELECT count(*) as instance_count FROM cfsb.instances WHERE effective_at IS NOT NULL AND decommissioned_at IS NULL; `
+			rowCount, err := getRowCount(address, sq)
+			sc2InstanceCount = append(sc2InstanceCount, rowCount)
+			Expect(err).NotTo(HaveOccurred())
+			fmt.Printf("%s: Found %d instances...\n", rdpgsc2Nodes[i].Node, rowCount)
+		}
+		//Verify each database also sees the same number of records (bdr sanity check)
+		for i := 1; i < len(sc2InstanceCount); i++ {
+			Expect(sc2InstanceCount[0]).To(Equal(sc2InstanceCount[i]))
+		}
+
+		//Check MC
+		var mcInstanceCount []int
+		for i := 0; i < len(rdpgmcNodes); i++ {
+			address := rdpgmcNodes[i].Address
+			sq := ` SELECT count(*) as instance_count FROM cfsb.instances WHERE effective_at IS NOT NULL AND decommissioned_at IS NULL; `
+			rowCount, err := getRowCount(address, sq)
+			mcInstanceCount = append(mcInstanceCount, rowCount)
+			Expect(err).NotTo(HaveOccurred())
+			fmt.Printf("%s: Found %d instances...\n", rdpgmcNodes[i].Node, rowCount)
+		}
+		//Verify each database also sees the same number of records (bdr sanity check)
+		for i := 1; i < len(mcInstanceCount); i++ {
+			Expect(mcInstanceCount[0]).To(Equal(mcInstanceCount[i]))
+		}
+
+		//Verify that the number of instances seen in the Management Cluster is the
+		//sum of the instances from the service ClusterIPs
+
+		totalManagementClusterInstances := mcInstanceCount[0]
+		totalServiceClusterInstances := sc1InstanceCount[0] + sc2InstanceCount[0]
+		Expect(totalManagementClusterInstances).To(Equal(totalServiceClusterInstances))
+		fmt.Printf("Total Management Cluster Instance Count: %d\n", totalManagementClusterInstances)
+		fmt.Printf("Total Service Cluster Instance Count: %d\n", totalServiceClusterInstances)
+	})
+
+	It("Check Scheduled Tasks Exist", func() {
+
+		rdpgmcNodes := getNodesByClusterName("rdpgmc")
+		rdpgsc1Nodes := getNodesByClusterName("rdpgsc1")
+		rdpgsc2Nodes := getNodesByClusterName("rdpgsc2")
+
+		fmt.Println(rdpgsc1Nodes)
+
+		//Check SC1
+		var sc1RowCount []int
+		for i := 0; i < len(rdpgsc1Nodes); i++ {
+			address := rdpgsc1Nodes[i].Address
+			sq := ` SELECT count(*) AS rowCount FROM tasks.schedules WHERE role IN ('all', 'service'); `
+			rowCount, err := getRowCount(address, sq)
+			sc1RowCount = append(sc1RowCount, rowCount)
+			Expect(err).NotTo(HaveOccurred())
+			fmt.Printf("%s: Found %d scheduled tasks...\n", rdpgsc1Nodes[i].Node, rowCount)
+		}
+		//Verify each database also sees the same number of records (bdr sanity check)
+		for i := 1; i < len(sc1RowCount); i++ {
+			Expect(sc1RowCount[0]).To(Equal(sc1RowCount[i]))
+		}
+
+		Expect(sc1RowCount[0]).To(BeNumerically(">=", 3))
+
+		//Check SC2
+		var sc2RowCount []int
+		for i := 0; i < len(rdpgsc2Nodes); i++ {
+			address := rdpgsc2Nodes[i].Address
+			sq := ` SELECT count(*) AS rowCount FROM tasks.schedules WHERE role IN ('all', 'service'); `
+			rowCount, err := getRowCount(address, sq)
+			sc2RowCount = append(sc2RowCount, rowCount)
+			Expect(err).NotTo(HaveOccurred())
+			fmt.Printf("%s: Found %d scheduled tasks...\n", rdpgsc2Nodes[i].Node, rowCount)
+		}
+		//Verify each database also sees the same number of records (bdr sanity check)
+		for i := 1; i < len(sc2RowCount); i++ {
+			Expect(sc2RowCount[0]).To(Equal(sc2RowCount[i]))
+		}
+
+		Expect(sc2RowCount[0]).To(BeNumerically(">=", 3))
+
+		//Check MC
+		var mcRowCount []int
+		for i := 0; i < len(rdpgmcNodes); i++ {
+			address := rdpgmcNodes[i].Address
+			sq := ` SELECT count(*) AS rowCount FROM tasks.schedules WHERE role IN ('all', 'manager'); `
+			rowCount, err := getRowCount(address, sq)
+			mcRowCount = append(mcRowCount, rowCount)
+			Expect(err).NotTo(HaveOccurred())
+			fmt.Printf("%s: Found %d scheduled tasks...\n", rdpgmcNodes[i].Node, rowCount)
+		}
+		//Verify each database also sees the same number of records (bdr sanity check)
+		for i := 1; i < len(mcRowCount); i++ {
+			Expect(mcRowCount[0]).To(Equal(mcRowCount[i]))
+		}
+
+		Expect(mcRowCount[0]).To(BeNumerically(">=", 4))
+
+	})
+
+	It("Check For Missed Scheduled Tasks", func() {
+
+		//Looks for any active scheduled tasks which have not been scheduled in two
+		//frequency cycles
+
+		allNodes := getAllNodes()
+
+		//Check all nodes
+		var nodeRowCount []int
+		for i := 0; i < len(allNodes); i++ {
+			address := allNodes[i].Address
+			sq := ` SELECT count(*) as rowCount FROM tasks.schedules WHERE last_scheduled_at + (2 * frequency) < CURRENT_TIMESTAMP AND enabled=true; `
+			rowCount, err := getRowCount(address, sq)
+			nodeRowCount = append(nodeRowCount, rowCount)
+			Expect(err).NotTo(HaveOccurred())
+			fmt.Printf("%s: Found %d missed scheduled tasks...\n", allNodes[i].Node, rowCount)
+		}
+		//Verify each database also sees the same number of records (bdr sanity check)
+		for i := 1; i < len(nodeRowCount); i++ {
+			Expect(nodeRowCount[0]).To(Equal(nodeRowCount[i]))
+		}
+
+		Expect(len(nodeRowCount)).NotTo(Equal(0))
+		//There should be no rows which have missed their schedule twice
+		Expect(nodeRowCount[0]).To(Equal(0))
+
+	})
+
+	It("Check for databases known to cfsb.instances but don't exist", func() {
+
+		allNodes := getServiceNodes()
+
+		//Check SC
+		var scRowCount []int
+		for i := 0; i < len(allNodes); i++ {
+			address := allNodes[i].Address
+			sq := `SELECT count(name) AS rowCount FROM ( (SELECT dbname AS name FROM cfsb.instances) EXCEPT (SELECT datname AS name FROM pg_database WHERE datname LIKE 'd%') ) AS instances_missing_databaes; `
+			rowCount, err := getRowCount(address, sq)
+			scRowCount = append(scRowCount, rowCount)
+			Expect(err).NotTo(HaveOccurred())
+			fmt.Printf("%s: Found %d databases known to cfsb.instances but don't exist...\n", allNodes[i].Node, rowCount)
+		}
+		//Verify each database also sees the same number of records (bdr sanity check)
+		for i := 1; i < len(scRowCount); i++ {
+			Expect(scRowCount[0]).To(Equal(scRowCount[i]))
+		}
+
+		Expect(len(allNodes)).NotTo(Equal(0))
+		//There should be no rows of databases which are known to cfsb.instances but don't exist
+		Expect(scRowCount[0]).To(Equal(0))
+
+	})
+
+	It("Check for databases which exist and aren't known to cfsb.instances", func() {
+
+		allNodes := getServiceNodes()
+
+		//Check SC
+		var scRowCount []int
+		for i := 0; i < len(allNodes); i++ {
+			address := allNodes[i].Address
+			sq := `SELECT count(name) as rowCount FROM ( (SELECT datname AS name FROM pg_database WHERE datname LIKE 'd%') EXCEPT (SELECT dbname AS name FROM cfsb.instances)) AS databases_missing_instances; `
+			rowCount, err := getRowCount(address, sq)
+			scRowCount = append(scRowCount, rowCount)
+			Expect(err).NotTo(HaveOccurred())
+			fmt.Printf("%s: Found %d databases in pg not in cfsb.instances...\n", allNodes[i].Node, rowCount)
+		}
+		//Verify each database also sees the same number of records (bdr sanity check)
+		for i := 1; i < len(scRowCount); i++ {
+			Expect(scRowCount[0]).To(Equal(scRowCount[i]))
+		}
+
+		Expect(len(allNodes)).NotTo(Equal(0))
+
+		//There should be no rows of databases which are known to pg but aren't in cfsb.instances
+		Expect(scRowCount[0]).To(Equal(0))
+
+	})
+
+})
